012af32934ae46e277305ec2b75f4f85af97b5d5
aniketsinha5552 <104712880+aniketsinha5552@users.noreply.github.com>
2024-06-17 12:09:28 +0530

    supabase and create embeddings api

--- src/app/api/embed/route.js
+++ src/app/api/embed/route.js
@@ -1,12 +1,45 @@
 import { NextResponse } from "next/server";
 import { splitter } from "../../../utils/splitter"
+import { createClient } from "@supabase/supabase-js";
+import {SupabaseVectorStore} from "langchain/vectorstores/supabase"
+import  {OpenAIEmbeddings} from "langchain/embeddings/openai";
+import { v4 as uuidv4 } from 'uuid';
 
 export const POST = async(req,res)=>{
     const body = await req.json()
 
     const text = body.text;
     const data = await splitter(text)
-    // console.log(splitter(text))
+
+    const sbAPIKey = process.env.SUPABASE_KEY;
+    const sbUrl = process.env.SUPABASE_URL;
+    const openAIKey = process.env.OPENAI_KEY;
+
+    // Add file id to data
+    const documentsWithIds = data.map(doc => ({
+        pageContent: doc.pageContent,
+        metadata: {
+            ...doc.metadata,
+            fileId : 999
+        }
+    }));
+    
+
+    const client = createClient(sbUrl,sbAPIKey);
+    try {
+        await SupabaseVectorStore.fromDocuments(
+          documentsWithIds,
+          new OpenAIEmbeddings({ openAIApiKey: openAIKey }),
+          {
+            client: client,
+            tableName: "documents",
+          }
+        );
+      } catch (e) {
+        console.log(e);
+      }
+    
+      // console.log(text)
 
     return NextResponse.json({data:data},{status:200})
 }

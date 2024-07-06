7dabb92d96d4dbc208a2bb34c6bc150d065ad28f
aniketsinha5552 <104712880+aniketsinha5552@users.noreply.github.com>
2024-07-06 13:02:33 +0530

    New: profile update page and api

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -28,3 +28,31 @@ export const GET=async(req:Request,res:Response)=>{
     }
 
 }
+
+
+export const POST=async(req:Request,res:Response)=>{
+    const session = await getAuthSession();
+    if(!session){
+        return NextResponse.json({error: "Unauthorized"}, {status: 401})
+    }
+
+    const body = await req.json()
+    const userId = body.userId
+    let prisma = new PrismaClient()
+     
+    try{
+        const currUser = await prisma.user.update({
+            where: {
+                id: userId,
+              },
+              data: {
+                name: body.name,
+              },
+        })
+        await prisma.$disconnect()
+        return NextResponse.json(currUser)
+
+    }catch(e:any){
+        return NextResponse.json({"error": "Some Error Occured"}, {status:500})
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,14 +1,29 @@
 "use client"
 import Loading from '@/components/Loading'
+import { Button } from '@/components/ui/button'
 import axios from 'axios'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
+import { useForm, SubmitHandler } from "react-hook-form"
+import { toast, ToastContainer } from 'react-toastify'
 
 
 
 const Profile = () => {
 
   let [user,setUser] = useState<any>(null)
+
+  const {register,handleSubmit} = useForm()
+
+  const notify=()=>toast("Profile Updated!")
+
+  const onsubmit=async(data:any)=>{
+    let res = await axios.post("/api/profile",{
+      userId:user.id ,
+      name: data.name
+    })
+    notify()
+  }
 
   const getUser = async()=>{
     try{
@@ -30,15 +45,22 @@ const Profile = () => {
     <div className='min-h-screen'>
       {
         user?
-        <div className='flex flex-col items-center p-10 w-fit m-auto'>
-        <Image src={user.image} alt='' width={200} height={200} className='rounded-full' />
-        <h1 className='text-start w-full text-2xl font-bold mt-4'>{user.name}</h1>
-        <h1 className='text-start w-full '>{user.email}</h1>
-        <p className='text-start w-full '>Files uploaded: {user.files.length}</p>
-      </div>: 
+        <form className='flex flex-col items-center p-10 gap-3 w-fit m-auto' onSubmit={handleSubmit(onsubmit)}>
+          <Image src={user.image} alt='' width={200} height={200} className='rounded-full' />
+           <input  className='text-start w-full border-2 border-gray-400 p-2 bg-transparent rounded-md text-xl' defaultValue={user.name} {...register("name")} />
+           <input disabled className='text-start w-full border-2 border-gray-400 p-2  bg-transparent rounded-md text-md' defaultValue={user.email} {...register("email")} />
+           <p className='text-start w-full border-2 border-gray-400 p-2 rounded-md '>Files uploaded: {user.files.length}</p>
+
+           {/* <input defaultValue={user.name} {...register("name")} /> */}
+           <Button>Save</Button>
+            {/* <h1 className='text-start w-full text-2xl font-bold mt-4'>{user.name}</h1>
+            <h1 className='text-start w-full '>{user.email}</h1>
+            <p className='text-start w-full '>Files uploaded: {user.files.length}</p> */}
+        </form>
+   : 
       <Loading/>
       }
-   
+     <ToastContainer/>
     </div>
   )
 }
